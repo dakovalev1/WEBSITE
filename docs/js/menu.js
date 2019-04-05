013c15e63bb6ff9fa9223e002fe23c2f79c6d2ab
Dmitry Kovalev <dakovalev1@mail.ru>
2019-04-05 14:53:58 +0300

    finished menu style breakpoints

--- docs/js/menu.js
+++ docs/js/menu.js
@@ -3,18 +3,33 @@ $(document).ready(function(){
     var menu_toggle = false;
 
     $("ul.menu li.menu-button a").click(function(){
-        if (menu_toggle){
-            $("ul.menu").css("height", "50px");
-        } else {
-            $("ul.menu").css("height", "200px");
-        }
         menu_toggle = !menu_toggle;
+        $("ul.menu").stop(true);
+        if (menu_toggle){
+            $("ul.menu").animate({height: "200px"});
+        } else {
+            $("ul.menu").animate({height: "50px"});
+        }
         return false;
     });
     
     $("ul.menu li.menu-item a").click(function(){
         menu_toggle = false;
         $("ul.menu").css("height", "50px");
+    });
+
+    $(window).resize(function(){
+        if ($("li.menu-button").css("display") == "none"){
+            $("ul.menu").stop(true);
+            $("ul.menu").css("height", "70px");
+            menu_toggle = false;
+        } else {
+            if (menu_toggle){
+                $("ul.menu").css("height", "200px");
+            } else {
+                $("ul.menu").css("height", "50px");
+            }
+        }
     });
 });
 
